434cd92689d99dd57fd99e059a77d728095c46e5
Julio Bermudez <jbermudezfc90@gmail.com>
2020-05-24 10:34:41 -0400

    add models

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    title: { type: String, minlength: 3, required: true },
+    timestamp: { type: Date, required: true },
+    text: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: { type: String, minlength: 2, required: true },
+    last_name: { type: String, minlength: 2, required: true },
+    username: { type: String, minlength: 4, required: true },
+    password: { type: String, required: true, minlength: 6 },
+    member: { type: Boolean, required: true },
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+});
+
+UserSchema.virtual('full_name').get(function () {
+    return `${this.first_name} ${this.last_name}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
